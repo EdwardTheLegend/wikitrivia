61a5b19e42f4468b9eb443145810829bdd9ccbff
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2022-04-25 18:28:04 +0800

    remove comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Index() {
           href="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20100%20100%22%3E%3Ctext%20y%3D%22.9em%22%20font-size%3D%2290%22%3E%F0%9F%8F%9B%EF%B8%8F%3C%2Ftext%3E%3C%2Fsvg%3E"
           type="image/svg+xml"
         />
-        <!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "ee32d467b63d4a668b929bfb0f965c29"}'></script><!-- End Cloudflare Web Analytics -->
+        <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "ee32d467b63d4a668b929bfb0f965c29"}'></script>
       </Head>
 
       <Game />
