9b704126cdccb7de2b536dbdfa68e838611ee4e6
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2022-05-14 16:42:50 +0800

    remove media="print"

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Index() {
 
         <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="anonymous" />
         <link rel="preload" as="style" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" media="print" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
         <noscript>
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
         </noscript>
