163f3c2983e28763536a509e02a43235c93ba308
EdwardTheLegend <40855056+EdwardTheLegend@users.noreply.github.com>
2022-05-19 16:14:01 +0800

    add font to head for home button

--- components/home-button.tsx
+++ components/home-button.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 import styles from "../styles/home-button.module.scss";
 import Link from "next/link";
+import Head from "next/head";
 
 export default function HomeButton() {
   return (
-    <Link href="/">
-      <button className={styles.homeButton}>home</button>
-    </Link>
+    <>
+      <Head>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com/"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+        <noscript>
+          <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet"
+          />
+        </noscript>
+      </Head>
+      <Link href="/">
+        <button className={styles.homeButton}>home</button>
+      </Link>
+    </>
   );
 }
